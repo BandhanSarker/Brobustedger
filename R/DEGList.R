00072ff3653b7f0346861cd897bf04dee27e294d
Bandhan Sarker <85154432+BandhanSarker@users.noreply.github.com>
2025-05-21 22:33:38 +0800

    Update DEGList.R

--- R/DEGList.R
+++ R/DEGList.R
@@ -76,7 +76,58 @@ DEGList<-function(data,n1,n2,p.threshold=0.05,padjust="BH",mi=5,NT=100){
 
   ########Imputation of nullify values using RF############
   m<-NI(dataM)
-
+  ## Added
+  decideTestsDGE <- function(object,adjust.method="BH",p.value=0.05,lfc=0)
+  #	Accept or reject hypothesis tests across genes and contrasts
+  #	edgeR team. Original author was Davis McCarthy.
+  #	Created 15 August 2010. Last modified 15 July 2018.
+{
+  #	Check object class
+  if( !(is(object,"DGEExact") || is(object,"DGELRT")) ) stop("Need DGEExact or DGELRT object")
+  
+  #	Apply multiple testing
+  p <- object$table$PValue
+  p <- p.adjust(p, method=adjust.method)
+  isDE <- as.integer(p < p.value)
+  
+  #	Extract logFC
+  logFC <- object$table$logFC
+  
+  #	Check for F-test with multiple logFC columns
+  FTest <- is.null(logFC)
+  
+  #	With multiple contrasts, apply lfc threshold to maximum logFC
+  if(FTest) {
+    if(lfc>0) {
+      coef.col <- grep("^logFC",colnames(object$table))
+      logFC <- object$table[,coef.col]
+      SmallFC <- rowSums(abs(logFC) >= lfc) == 0
+      isDE[SmallFC] <- 0L
+    }
+    
+    #	With single contrast, apply directionality and lfc threshold
+  } else {
+    isDE[isDE & logFC<0] <- -1L
+    SmallFC <- (abs(logFC) < lfc)
+    isDE[SmallFC] <- 0L
+  }
+  
+  #	Assemble TestResults object
+  isDE <- matrix(isDE, ncol=1)
+  row.names(isDE) <- row.names(object)
+  colnames(isDE) <- paste(rev(object$comparison),collapse="-")
+  
+  #	Record possible values
+  if(FTest) {
+    attr(isDE,"levels") <- c(0L,1L)
+    attr(isDE,"labels") <- c("NotSig","Sig")
+  } else {
+    attr(isDE,"levels") <- c(-1L,0L,1L)
+    attr(isDE,"labels") <- c("Down","NotSig","Up")
+  }		
+  
+  new("TestResults", isDE)
+}
   ########
   dataR<-round(m,0)
   # Assign condition
@@ -104,7 +155,7 @@ DEGList<-function(data,n1,n2,p.threshold=0.05,padjust="BH",mi=5,NT=100){
   lrt.edgeR <- edgeR::glmLRT(fit.edgeR, contrast=contrasts.edgeR)
   #logfold
   edgeR_results <- lrt.edgeR$table
-  sig.edgeR <- edgeR::decideTests(lrt.edgeR, adjust.method=padjust, p.value = p.threshold)
+  sig.edgeR <- decideTestsDGE(lrt.edgeR, adjust.method=padjust, p.value = p.threshold)
   genes.edgeR <- row.names(edgeR_results)[which(sig.edgeR != 0)]
   result<-edgeR_results[genes.edgeR,]
   result$p.adjust<-p.adjust(result$PValue, method =padjust, n = length(genes.edgeR))
